64471c1c3441d87f848af77faddab02e2f439d88
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-14 23:36:18 +0200

    setup route

--- lib/src/core/config/routes/app_route.dart
+++ lib/src/core/config/routes/app_route.dart
@@ -1,12 +1,12 @@
+import 'package:aagel/src/presentation/screens/home/home_screen_page.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../main.dart';
 
 class AppRoute {
-  Route generateRoute(RouteSettings route) {
+  static Route generateRoute(RouteSettings route) {
     switch (route.name) {
       default:
-        return MaterialPageRoute(builder: (_) => MyHomePage(title: 'Main',));
+        return MaterialPageRoute(builder: (_) => const HomeScreenPage());
     }
   }
 }
