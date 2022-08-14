f43e93a1266719ce8f2f653f5d5bd76b1bb7bf72
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-14 23:36:32 +0200

    add constant

---
+++ lib/src/core/utils/constant.dart
@@ -0,0 +1,10 @@
+class Constant{
+  
+  static const String kBaseUrl = "https://newsapi.org/v2/";
+  static const String kApiKey = "6faa213056254af2a6383ed811dcd50d";
+  
+  static const String kThemeModeKey = 'mode';
+  static const String kLocaleLanguageCode = 'locale.language';
+  static const String kLocaleCountryCode = 'locale.country';
+
+}
