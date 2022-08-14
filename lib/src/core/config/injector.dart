33061fb41cb75640cc0e22629816eccb2493e50f
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-14 23:36:37 +0200

    ----

--- lib/src/core/config/injector.dart
+++ lib/src/core/config/injector.dart
@@ -1,20 +1,27 @@
-import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:aagel/src/data/data_sources/remote/api_service.dart';
-import 'package:aagel/src/data/data_sources/remote/dio_clinet.dart';
 import 'package:aagel/src/data/repositories/app_repository_imp.dart';
 import 'package:aagel/src/presentation/blocs/api_data_bloc.dart';
 
 import '../../domain/repositories/i_app_repository.dart';
+import '../services/dio_service.dart';
+import '../services/setting_service.dart';
+import '../services/storage_service.dart';
 
 
 final injector = GetIt.instance;
 
 Future<void> initializeDependencies() async {
 
-  injector.registerLazySingleton<Dio>(() => DioClient.initialDio());
+  final dioService = await DioService().initialize();
+
+  injector.registerLazySingleton<DioService>(() => dioService);
+  injector.registerLazySingleton<StorageService>(() => StorageService());
   injector.registerLazySingleton<ApiService>(() => ApiService(injector()));
   injector.registerLazySingleton<IAppRepository>(() => AppRepositoryImp(injector()));
   injector.registerFactory<ApiDataBloc>(() => ApiDataBloc());
+
+  await StorageService().initialize();
+  await SettingService().initialize();
 
 }
