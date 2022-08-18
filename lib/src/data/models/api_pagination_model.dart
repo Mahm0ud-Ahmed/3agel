518c2093f781b752446ea6602c89d60b80f18584
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-18 22:44:19 +0200

    update model

--- lib/src/data/models/api_pagination_model.dart
+++ lib/src/data/models/api_pagination_model.dart
@@ -1,36 +1,37 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 import 'package:reflectable/reflectable.dart';
+
 import 'package:aagel/src/core/utils/reflector.dart';
 
 @ModelReflector()
-class ApiPaginationModel<MODEL> extends Equatable{
+class ApiPaginationModel<MODEL> extends Equatable {
   List<MODEL>? data;
-  int? total;
-  dynamic skip;
-  int? limit;
+  int? totalResults;
+  String? status;
 
-  ApiPaginationModel({this.data, this.total, this.skip, this.limit});
+  ApiPaginationModel({
+    this.data,
+    this.totalResults,
+    this.status,
+  });
+
+
 
   factory ApiPaginationModel.fromJson(Map<String, dynamic> json) {
 
     ClassMirror classMirror = modelReflector.reflectType(MODEL) as ClassMirror;
     List<MODEL> data = [];
-    for (var element in (json['products'] as List)) {
+    for (var element in (json["articles"] as List)) {
       data.add(classMirror.newInstance('fromJson', [element]) as MODEL);
     }
 
     return ApiPaginationModel<MODEL>(
-      limit: json['limit'],
-      skip: json['skip'],
-      total: json['total'],
+      totalResults: json['totalResults'],
+      status: json['status'],
       data: data,
     );
   }
   @override
-  List<Object?> get props => [data, total, skip, limit];
-
-  reflectObject(Map<String, dynamic> json){
-    ClassMirror classMirror = modelReflector.reflectType(MODEL) as ClassMirror;
-    return classMirror.newInstance('fromJson', [json]) as MODEL;
-  }
+  List<Object?> get props => [data, totalResults, status];
 }
---
+++ lib/src/data/models/article_model.dart
@@ -0,0 +1,38 @@
+
+import 'package:aagel/src/core/utils/reflector.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'article_model.g.dart';
+
+@JsonSerializable()
+@ModelReflector()
+class ArticleModel {
+
+  @JsonKey(ignore: true)
+  final String route = 'top-headlines';
+
+  final Map<String, dynamic>? source;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publishedAt;
+  final String? content;
+
+  ArticleModel({
+    this.source,
+    this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+
+  factory ArticleModel.fromJson(Map<String, dynamic> json) => _$ArticleModelFromJson(json);
+
+  Map<String, dynamic> toMap() => _$ArticleModelToJson(this);
+
+}
