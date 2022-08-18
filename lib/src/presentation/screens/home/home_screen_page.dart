e01238dfb9ff19fa1dd99b4406e843c89698678e
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-18 22:44:31 +0200

    Update home_screen_page.dart

--- lib/src/presentation/screens/home/home_screen_page.dart
+++ lib/src/presentation/screens/home/home_screen_page.dart
@@ -1,8 +1,12 @@
 import 'package:aagel/src/core/config/themes/dark_theme.dart';
-import 'package:aagel/src/core/config/themes/light_theme.dart';
 import 'package:aagel/src/core/services/setting_service.dart';
 import 'package:aagel/src/core/services/storage_service.dart';
+import 'package:aagel/src/core/utils/query_params.dart';
+import 'package:aagel/src/data/models/article_model.dart';
+import 'package:aagel/src/presentation/blocs/api_data_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 
 class HomeScreenPage extends StatefulWidget {
@@ -13,20 +17,28 @@ class HomeScreenPage extends StatefulWidget {
 }
 
 class _HomeScreenPageState extends State<HomeScreenPage> {
+  ApiDataBloc<ArticleModel> bloc = ApiDataBloc(query: QueryParams(category: 'general', pageSize: 5), maxResult: 5);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: FloatingActionButton(onPressed: () async{
-        await StorageService().saveBool('mode', false);
-        SettingService().changeThemeMode(LightTheme());
-      }),
+    return BlocProvider(
+      create: (context) => ApiDataBloc(),
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: PagedListView(
+            pagingController: bloc.controller, 
+            builderDelegate: PagedChildBuilderDelegate<ArticleModel> (
+              itemBuilder: (context, item, index) {
+                return Text(item.title.toString());
+              },
+            ),
+          )
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () async{
+          await StorageService().saveBool('mode', true);
+          SettingService().changeThemeMode(DarkTheme());
+        }),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () async{
-        await StorageService().saveBool('mode', true);
-        SettingService().changeThemeMode(DarkTheme());
-      }),
     );
   }
 }
