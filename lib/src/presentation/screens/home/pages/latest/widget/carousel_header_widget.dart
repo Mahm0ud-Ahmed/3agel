752a72cc3bd7f5f68586bc0ab484df0df75a63df
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-27 15:29:04 +0200

    add more state

--- lib/src/presentation/screens/home/pages/latest/widget/carousel_header_widget.dart
+++ lib/src/presentation/screens/home/pages/latest/widget/carousel_header_widget.dart
@@ -1,11 +1,7 @@
-import 'package:aagel/src/core/config/themes/theme_manager.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:aagel/src/presentation/screens/home/pages/latest/widget/carousel_item_loaded_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../../../../../core/config/assets/assets.gen.dart';
-import '../../../../../../core/config/assets/fonts.gen.dart';
+import '../../../../../../core/config/l10n/generated/l10n.dart';
 import '../../../../../../core/utils/query_params.dart';
 import '../../../../../../data/models/api_pagination_model.dart';
 import '../../../../../../data/models/article_model.dart';
@@ -17,8 +13,6 @@ class CarouselHeaderWidget extends StatelessWidget {
       query: QueryParams(category: 'sport', pageSize: 5), maxResult: 5)
     ..add(ApiDataPagination());
 
-  final ValueNotifier<int> _notifier = ValueNotifier<int>(0);
-
   CarouselHeaderWidget({super.key});
 
   @override
@@ -29,116 +23,27 @@ class CarouselHeaderWidget extends StatelessWidget {
       child: BlocBuilder(
         bloc: carouselBloc,
         builder: (context, state) {
-          if (state is ApiDataLoaded<ApiPaginationModel<ArticleModel>>) {
-            List<ArticleModel>? article = state.data!.data;
-            return Column(
-              children: [
-                CarouselSlider.builder(
-                  itemCount: state.data!.data?.length,
-                  options: CarouselOptions(
-                    height: 200,
-                    viewportFraction: 0.92,
-                    enableInfiniteScroll: true,
-                    reverse: false,
-                    autoPlay: true,
-                    autoPlayInterval: const Duration(seconds: 15),
-                    autoPlayAnimationDuration:
-                        const Duration(milliseconds: 800),
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    enlargeCenterPage: true,
-                    scrollDirection: Axis.horizontal,
-                    onPageChanged: (index, reason) {
-                      _notifier.value = index;
-                    },
-                  ),
-                  itemBuilder:
-                      (BuildContext context, int itemIndex, int pageViewIndex) {
-                    return ClipRRect(
-                      borderRadius: BorderRadius.circular(14),
-                      child: Stack(
-                        children: [
-                          CachedNetworkImage(
-                            imageUrl: article![itemIndex].urlToImage ?? '',
-                            width: double.infinity,
-                            height: double.infinity,
-                            fit: BoxFit.cover,
-                            placeholder: (context, url) => Assets.images.noImage
-                                .svg(
-                                    width: double.infinity,
-                                    height: double.infinity,
-                                    fit: BoxFit.cover),
-                            errorWidget: (context, url, error) =>
-                                Assets.images.noImage.svg(
-                                    width: double.infinity,
-                                    height: double.infinity,
-                                    fit: BoxFit.cover),
-                          ),
-                          Align(
-                            alignment: Alignment.bottomCenter,
-                            child: SizedBox(
-                              height: 80,
-                              width: double.infinity,
-                              child: DecoratedBox(
-                                decoration:
-                                    const BoxDecoration(color: Colors.black38),
-                                child: Padding(
-                                  padding: const EdgeInsetsDirectional.only(
-                                      top: 8, start: 8),
-                                  child: Text(
-                                    article[itemIndex].title!,
-                                    style: const TextStyle(
-                                        fontFamily: FontFamily.roboto,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w400,
-                                        overflow: TextOverflow.ellipsis,
-                                        height: 1.3,
-                                        color: Colors.white),
-                                    maxLines: 2,
-                                    softWrap: true,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(state.data!.data!.length, (index) {
-                    return SizedBox(
-                      width: 12,
-                      height: 8,
-                      child: Padding(
-                        padding: const EdgeInsetsDirectional.only(end: 4),
-                        child: ValueListenableBuilder(
-                          valueListenable: _notifier,
-                          builder: (context, value, child) {
-                            return DecoratedBox(
-                                decoration: BoxDecoration(
-                                    color: _notifier.value == index
-                                        ? ThemeManager().appColor[3]
-                                        : ThemeManager().appColor[5],
-                                    borderRadius: BorderRadius.circular(8)));
-                          },
-                        ),
-                      ),
-                    );
-                  }),
-                ),
-              ],
-            );
-          } else {
+
+          if (state is ApiDataLoaded<ApiPaginationModel<ArticleModel>> &&
+              (state.data?.data != null && state.data!.data!.isNotEmpty)) {
+            return CarouselItemLoadedWidget(article: state.data?.data);
+
+          } else if (state is ApiDataLoading) {
             return const CustomShimmerWidget(
               margin: EdgeInsets.symmetric(horizontal: 12),
               height: 200,
               width: double.infinity,
               radius: 14,
+            );
+
+          } else if (state is ApiDataError) {
+            return Center(
+              child: Text(state.error!.message ?? ''),
+            );
+            
+          } else {
+            return Center(
+              child: Text(S().no_more_data_state),
             );
           }
         },
---
+++ lib/src/presentation/screens/home/pages/latest/widget/carousel_item_loaded_widget.dart
@@ -0,0 +1,120 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../../core/config/assets/assets.gen.dart';
+import '../../../../../../core/config/assets/fonts.gen.dart';
+import '../../../../../../core/config/themes/theme_manager.dart';
+import '../../../../../../data/models/article_model.dart';
+
+class CarouselItemLoadedWidget extends StatelessWidget {
+  final List<ArticleModel>? article;
+  final ValueNotifier<int> _notifier = ValueNotifier<int>(0);
+
+  CarouselItemLoadedWidget({super.key, required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider.builder(
+          itemCount: article?.length,
+          options: CarouselOptions(
+            height: 200,
+            viewportFraction: 0.92,
+            enableInfiniteScroll: true,
+            reverse: false,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 15),
+            autoPlayAnimationDuration: const Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeCenterPage: true,
+            scrollDirection: Axis.horizontal,
+            onPageChanged: (index, reason) {
+              _notifier.value = index;
+            },
+          ),
+          itemBuilder:
+              (BuildContext context, int itemIndex, int pageViewIndex) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(14),
+              child: Stack(
+                children: [
+                  CachedNetworkImage(
+                    imageUrl: article![itemIndex].urlToImage ?? '',
+                    width: double.infinity,
+                    height: double.infinity,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) => Assets.images.noImage.svg(
+                        width: double.infinity,
+                        height: double.infinity,
+                        fit: BoxFit.cover),
+                    errorWidget: (context, url, error) => Assets.images.noImage
+                        .svg(
+                            width: double.infinity,
+                            height: double.infinity,
+                            fit: BoxFit.cover),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: SizedBox(
+                      height: 80,
+                      width: double.infinity,
+                      child: DecoratedBox(
+                        decoration: const BoxDecoration(color: Colors.black38),
+                        child: Padding(
+                          padding: const EdgeInsetsDirectional.only(
+                              top: 8, start: 8),
+                          child: Text(
+                            article![itemIndex].title!,
+                            style: const TextStyle(
+                                fontFamily: FontFamily.roboto,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                                overflow: TextOverflow.ellipsis,
+                                height: 1.3,
+                                color: Colors.white),
+                            maxLines: 2,
+                            softWrap: true,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(article!.length, (index) {
+            return SizedBox(
+              width: 12,
+              height: 8,
+              child: Padding(
+                padding: const EdgeInsetsDirectional.only(end: 4),
+                child: ValueListenableBuilder(
+                  valueListenable: _notifier,
+                  builder: (context, value, child) {
+                    return DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: _notifier.value == index
+                            ? ThemeManager().appColor[3]
+                            : ThemeManager().appColor[5],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            );
+          }),
+        ),
+      ],
+    );
+  }
+}
