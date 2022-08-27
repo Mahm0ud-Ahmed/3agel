8845c8fdb6660b9d18bb8a4b81e8ab18b28b3ef8
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-28 00:43:47 +0200

    Update constant.dart

--- lib/src/core/utils/constant.dart
+++ lib/src/core/utils/constant.dart
@@ -1,7 +1,8 @@
 class Constant{
   
   static const String kBaseUrl = "https://newsapi.org/v2/";
-  static const String kApiKey = "6faa213056254af2a6383ed811dcd50d";
+  // static const String kApiKey = "6faa213056254af2a6383ed811dcd50d";
+  static const String kApiKey = "0ba6d6edf8c2433882debab1551e9c30";
   
   static const String kThemeModeKey = 'mode';
   static const String kLocaleLanguageCode = 'locale.language';
