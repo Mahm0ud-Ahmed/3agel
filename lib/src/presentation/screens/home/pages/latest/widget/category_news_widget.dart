a347cecb8fc256a55a4e52cb3ec691fddd240605
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-28 10:58:34 +0200

    Update category_news_widget.dart

--- lib/src/presentation/screens/home/pages/latest/widget/category_news_widget.dart
+++ lib/src/presentation/screens/home/pages/latest/widget/category_news_widget.dart
@@ -11,47 +11,44 @@ class CategoryNewsWidget extends StatelessWidget {
   CategoryNewsWidget({super.key, this.onTab, required this.categoryListen});
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        physics: const BouncingScrollPhysics(),
-        child: Row(
-          children: List.generate(NewsCategory.values.length, (index) {
-            return Padding(
-              padding: const EdgeInsetsDirectional.only(end: 6),
-              child: GestureDetector(
-                onTap: () => onTab!.call(NewsCategory.values[index]),
-                child: SizedBox(
-                  height: 40,
-                  width: 108,
-                  child: ValueListenableBuilder(
-                    valueListenable: categoryListen,
-                    builder: (context, value, child) {
-                      return DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: categoryListen.value ==
-                                  NewsCategory.values[index]
-                              ? ThemeManager().appColor[3]
-                              : null,
-                          borderRadius: BorderRadius.circular(25),
-                          border: Border.all(
-                            color: ThemeManager().appColor[3],
-                            width: 2,
-                          ),
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      physics: const BouncingScrollPhysics(),
+      child: Row(
+        children: List.generate(NewsCategory.values.length, (index) {
+          return Padding(
+            padding: const EdgeInsetsDirectional.only(end: 6),
+            child: GestureDetector(
+              onTap: () => onTab!.call(NewsCategory.values[index]),
+              child: SizedBox(
+                height: 40,
+                width: 108,
+                child: ValueListenableBuilder(
+                  valueListenable: categoryListen,
+                  builder: (context, value, child) {
+                    return DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: categoryListen.value ==
+                                NewsCategory.values[index]
+                            ? ThemeManager().appColor[3]
+                            : null,
+                        borderRadius: BorderRadius.circular(25),
+                        border: Border.all(
+                          color: ThemeManager().appColor[3],
+                          width: 2,
                         ),
-                        child: Center(
-                          child: Text(toBeginningOfSentenceCase(
-                              NewsCategory.values[index].category)!),
-                        ),
-                      );
-                    },
-                  ),
+                      ),
+                      child: Center(
+                        child: Text(toBeginningOfSentenceCase(
+                            NewsCategory.values[index].category)!),
+                      ),
+                    );
+                  },
                 ),
               ),
-            );
-          }),
-        ),
+            ),
+          );
+        }),
       ),
     );
   }
