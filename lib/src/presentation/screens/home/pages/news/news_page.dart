f92771cb61e10564b0e162b87a3dbaf0515b5f74
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-08-28 22:04:44 +0200

    add new page

--- lib/src/presentation/screens/home/pages/news/news_page.dart
+++ lib/src/presentation/screens/home/pages/news/news_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/enums.dart';
+import '../../../../../core/utils/query_params.dart';
+import '../../../../../data/models/article_model.dart';
+import '../../../../controllers/data_bloc/api_data_bloc.dart';
+import '../latest/widget/category_news_list_widget.dart';
+import '../latest/widget/category_news_widget.dart';
 
 class NewsPage extends StatefulWidget {
   const NewsPage({super.key});
@@ -8,15 +14,59 @@ class NewsPage extends StatefulWidget {
 }
 
 class _NewsPageState extends State<NewsPage> {
-  // ApiDataBloc<ArticleModel> bloc = ApiDataBloc(query: QueryParams(category: 'general', pageSize: 5), maxResult: 5)..add(ApiDataPagination());
+  late final ApiDataBloc<ArticleModel> _articleBloc;
+  late final ValueNotifier<NewsCategory> _categoryListen;
+
+  final QueryParams _query = QueryParams(
+    category: NewsCategory.business.category,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    _articleBloc = ApiDataBloc(query: _query, maxResult: 20);
+    _categoryListen = ValueNotifier(NewsCategory.business);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: Colors.black,
-      child: Text('Bookmark')
+    return RefreshIndicator(
+      onRefresh: () async {
+        _articleBloc.controller.refresh();
+      },
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+            sliver: SliverToBoxAdapter(
+              child: CategoryNewsWidget(
+                onTab: updateListeners,
+                categoryListen: _categoryListen,
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            sliver: CategoryNewsList(articleBloc: _articleBloc),
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+        ],
+      ),
     );
+  }
 
+  void updateListeners(NewsCategory newCategory) {
+    if (newCategory != _categoryListen.value &&
+        newCategory.category != _query.category) {
+      _categoryListen.value = newCategory;
+      _query.category = newCategory.category;
+      _articleBloc.controller.refresh();
+    }
   }
 }
