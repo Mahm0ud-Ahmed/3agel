0837d48ce46d3483d05ec32fc1aea2aac544f842
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-09-03 17:29:26 +0200

    build text form field

---
+++ lib/src/presentation/widgets/generic_text_field.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../core/config/themes/theme_manager.dart';
+
+class GenericTextField extends StatelessWidget {
+  Function(String)? onChange;
+  Function()? onTab;
+  final EdgeInsetsGeometry margin;
+
+  GenericTextField({
+    super.key,
+    required this.margin,
+    this.onChange,
+    this.onTab,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: margin,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(25),
+        child: TextFormField(
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            filled: true,
+            fillColor: ThemeManager().appColor[5].withOpacity(0.1),
+            contentPadding: const EdgeInsetsDirectional.only(
+              top: 6,
+              start: 14,
+            ),
+            suffixIcon: GestureDetector(
+              onTap: onTab,
+              child: Icon(
+                Icons.search,
+                color: ThemeManager().appColor[5],
+              ),
+            ),
+          ),
+          onChanged: onChange,
+          onTap: onTab,
+        ),
+      ),
+    );
+  }
+}
