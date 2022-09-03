1ac460cec7adf87bb2c92f90ddd73bd3f48b10f7
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-09-03 17:30:09 +0200

    Update custom_container_with_icon.dart

--- lib/src/presentation/widgets/custom_container_with_icon.dart
+++ lib/src/presentation/widgets/custom_container_with_icon.dart
@@ -6,27 +6,34 @@ class CustomContainerWithIcon extends StatelessWidget {
   final double? height;
   final double? width;
   final Color? backgroundColor;
+  final EdgeInsetsGeometry? margin;
   final Widget child;
   const CustomContainerWithIcon({
     super.key,
     required this.child,
     this.height = 36,
     this.width = 36,
+    this.margin,
     this.backgroundColor,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: height,
-      width: width,
-      child: DecoratedBox(
+    return Padding(
+      padding: margin ?? EdgeInsets.zero,
+      child: SizedBox(
+        height: height,
+        width: width,
+        child: DecoratedBox(
           decoration: BoxDecoration(
-              color: backgroundColor ?? ThemeManager().appColor[3],
-              borderRadius: BorderRadius.circular(8)),
+            color: backgroundColor ?? ThemeManager().appColor[3],
+            borderRadius: BorderRadius.circular(8),
+          ),
           child: Center(
             child: child,
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
