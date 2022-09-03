7ddd313bf89ea9fe412f1f2c6c6f9f4581b95532
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-09-03 17:30:12 +0200

    Update search_page.dart

--- lib/src/presentation/screens/home/pages/search/search_page.dart
+++ lib/src/presentation/screens/home/pages/search/search_page.dart
@@ -1,4 +1,10 @@
+import 'package:aagel/src/core/config/themes/theme_manager.dart';
+import 'package:aagel/src/presentation/widgets/custom_container_with_icon.dart';
+import 'package:aagel/src/presentation/widgets/generic_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'component/header_search_widget.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -9,14 +15,15 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   // ApiDataBloc<ArticleModel> bloc = ApiDataBloc(query: QueryParams(category: 'general', pageSize: 5), maxResult: 5)..add(ApiDataPagination());
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: Colors.indigo,
-      child: Text('Bookmark')
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: HeaderSearchWidget(),
+        ),
+      ],
     );
-
   }
 }
