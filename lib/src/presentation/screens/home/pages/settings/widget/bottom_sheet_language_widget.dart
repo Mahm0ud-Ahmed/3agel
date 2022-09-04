a15d1e98ca8c4e6a250c59c28fea6ac800aee66d
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-09-04 14:57:30 +0200

    Update bottom_sheet_language_widget.dart

--- lib/src/presentation/screens/home/pages/settings/widget/bottom_sheet_language_widget.dart
+++ lib/src/presentation/screens/home/pages/settings/widget/bottom_sheet_language_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../../core/config/themes/theme_manager.dart';
 import '../../../../../../core/utils/enums.dart';
 
 class BottomSheetLanguageWidget extends StatelessWidget {
@@ -7,30 +8,35 @@ class BottomSheetLanguageWidget extends StatelessWidget {
 
   SupportLanguage? supportLang;
 
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 200,
-      child: Column(
-        children: [
-          RadioListTile(
-            value: SupportLanguage.english,
-            groupValue: supportLang,
-            onChanged: (SupportLanguage? value) {
-              supportLang = value;
-            },
-            title: Text(SupportLanguage.english.currentLang),
-          ),
-          RadioListTile(
-            value: SupportLanguage.arabic,
-            groupValue: supportLang,
-            onChanged: (SupportLanguage? value) {
-              supportLang = value;
-            },
-            title: Text(SupportLanguage.arabic.currentLang),
-          ),
-        ],
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: ThemeManager().appColor[0],
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(25)),
+        ),
+        child: Column(
+          children: [
+            RadioListTile(
+              value: SupportLanguage.english,
+              groupValue: supportLang,
+              onChanged: (SupportLanguage? value) {
+                supportLang = value;
+              },
+              title: Text(SupportLanguage.english.currentLang),
+            ),
+            RadioListTile(
+              value: SupportLanguage.arabic,
+              groupValue: supportLang,
+              onChanged: (SupportLanguage? value) {
+                supportLang = value;
+              },
+              title: Text(SupportLanguage.arabic.currentLang),
+            ),
+          ],
+        ),
       ),
     );
   }
