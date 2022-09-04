cd2c8a67f9ad34ee2e1fe4fd0009fe565b0dc3d7
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-09-04 12:09:12 +0200

    Create search_body_widget.dart

---
+++ lib/src/presentation/screens/home/pages/search/component/search_body_widget.dart
@@ -0,0 +1,86 @@
+import 'package:aagel/src/core/config/assets/assets.gen.dart';
+import 'package:aagel/src/data/models/article_model.dart';
+import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+import '../../../../../controllers/data_bloc/api_data_bloc.dart';
+import '../../../../../widgets/custom_shimmer_widget.dart';
+import '../../../../../widgets/category_news_item_widget.dart';
+
+class SearchBodyWidget extends StatelessWidget {
+  final ApiDataBloc<ArticleModel> searchBloc;
+  final ValueNotifier<bool?> notifier;
+  const SearchBodyWidget({
+    super.key,
+    required this.searchBloc,
+    required this.notifier,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: notifier,
+      builder: (context, value, child) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            !notifier.value!
+                ? AspectRatio(
+                    aspectRatio: 1,
+                    child: Assets.images.searchingData.svg(),
+                  )
+                : Expanded(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: PagedGridView(
+                            pagingController: searchBloc.controller,
+                            physics: const BouncingScrollPhysics(),
+                            gridDelegate:
+                                const SliverGridDelegateWithMaxCrossAxisExtent(
+                              maxCrossAxisExtent: 400,
+                              mainAxisExtent: 120,
+                              crossAxisSpacing: 8,
+                              mainAxisSpacing: 8,
+                            ),
+                            builderDelegate: PagedChildBuilderDelegate<ArticleModel>(
+                              itemBuilder: (context, item, index) {
+                                return CategoryNewsItemWidget(
+                                  article: item,
+                                );
+                              },
+                              firstPageProgressIndicatorBuilder: (context) {
+                                return Column(
+                                  children: List.generate(
+                                    10,
+                                    (index) => const CustomShimmerWidget(
+                                      height: 120,
+                                      width: 400,
+                                      radius: 16,
+                                    ),
+                                  ),
+                                );
+                              },
+                              noItemsFoundIndicatorBuilder: (context) {
+                                return Assets.images.noData.svg();
+                              },
+                              newPageProgressIndicatorBuilder: (context) {
+                                return const CustomShimmerWidget(
+                                  height: 120,
+                                  width: 400,
+                                  radius: 16,
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 100,)
+                      ],
+                    ),
+                  ),
+          ],
+        );
+      },
+    );
+  }
+}
