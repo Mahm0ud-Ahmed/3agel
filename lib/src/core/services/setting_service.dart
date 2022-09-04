e5821f74b20e03aaa178fc13f30894dd18275c60
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-09-04 14:12:11 +0200

    Update setting_service.dart

--- lib/src/core/services/setting_service.dart
+++ lib/src/core/services/setting_service.dart
@@ -1,16 +1,17 @@
 import 'package:aagel/src/core/config/themes/theme_manager.dart';
 import 'package:aagel/src/core/services/service_interface.dart';
+import 'package:flutter/material.dart';
 
-class SettingService implements ServiceInterface{
-
+class SettingService implements ServiceInterface {
   @override
   String get name => "Setting";
   late final ThemeManager _themeManager;
-  
+
   @override
-  Future<void> initialize() async{
+  Future<void> initialize() async {
     _themeManager = ThemeManager();
     _themeManager.load();
   }
 
+  static ThemeMode get stateMod => ThemeManager().mode;
 }
