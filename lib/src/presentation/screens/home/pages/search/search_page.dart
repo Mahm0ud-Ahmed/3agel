c7d0554961d5fcc8ee11bbcb790fb1827e8e09b8
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2022-09-04 12:09:17 +0200

    Update search_page.dart

--- lib/src/presentation/screens/home/pages/search/search_page.dart
+++ lib/src/presentation/screens/home/pages/search/search_page.dart
@@ -1,21 +1,62 @@
-import 'package:aagel/src/core/config/assets/assets.gen.dart';
-import 'package:aagel/src/core/config/themes/theme_manager.dart';
-import 'package:aagel/src/presentation/widgets/custom_container_with_icon.dart';
-import 'package:aagel/src/presentation/widgets/generic_text_field.dart';
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import '../../../../../core/utils/query_params.dart';
+import '../../../../../data/models/article_model.dart';
+import '../../../../controllers/data_bloc/api_data_bloc.dart';
 import 'component/header_search_widget.dart';
+import 'component/search_body_widget.dart';
 
 class SearchPage extends StatefulWidget {
-  const SearchPage({super.key});
+  bool isSearch;
+
+  SearchPage({super.key, this.isSearch = false});
 
   @override
   State<SearchPage> createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
-  // ApiDataBloc<ArticleModel> bloc = ApiDataBloc(query: QueryParams(category: 'general', pageSize: 5), maxResult: 5)..add(ApiDataPagination());
+  final QueryParams _query = QueryParams(pageSize: 10);
+  late ApiDataBloc<ArticleModel> _searchBloc;
+  Timer? _debounce;
+
+  String? currentSearch;
+
+  late final ValueNotifier<bool> _notifier;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.isSearch = false;
+    _notifier = ValueNotifier<bool>(widget.isSearch);
+    // _query = QueryParams(pageSize: 10);
+    _searchBloc = ApiDataBloc<ArticleModel>(query: _query, maxResult: 50);
+  }
+
+  @override
+  void dispose() {
+    _debounce?.cancel();
+    super.dispose();
+  }
+
+  executeSearch(String? searchValue) {
+    String? temp = searchValue?.trim();
+
+    if (_debounce?.isActive ?? false) _debounce?.cancel();
+    _debounce = Timer(const Duration(seconds: 1), () {
+      if ((temp != null && temp.isNotEmpty && temp.length > 1) &&
+          currentSearch != temp) {
+        currentSearch = temp;
+        _query.queryWord = temp;
+        _notifier.value = widget.isSearch = true;
+        _searchBloc.controller.refresh();
+      } else if (temp == null || temp.isEmpty) {
+        _notifier.value = widget.isSearch = false;
+        currentSearch = null;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,12 +64,29 @@ class _SearchPageState extends State<SearchPage> {
       physics: const BouncingScrollPhysics(),
       slivers: [
         SliverToBoxAdapter(
-          child: HeaderSearchWidget(),
+          child: HeaderSearchWidget(
+            onChange: executeSearch,
+            onTab: () {
+              if (currentSearch != null && currentSearch!.isNotEmpty) {
+                _notifier.value = widget.isSearch = true;
+                _searchBloc.controller.refresh();
+              }
+            },
+          ),
         ),
-        SliverFillRemaining(
-          hasScrollBody: false,
-          fillOverscroll: true,
-          child: Assets.images.searchingData.svg(),
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+          sliver: SliverFillViewport(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 1,
+              (context, index) {
+                return SearchBodyWidget(
+                  notifier: _notifier,
+                  searchBloc: _searchBloc,
+                );
+              },
+            ),
+          ),
         ),
       ],
     );
