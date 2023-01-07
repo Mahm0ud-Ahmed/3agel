9addf1a2f2938f14b63a4466edb601fc70d5a434
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2023-01-07 03:22:07 +0200

    Update error_handler.dart

--- lib/src/core/error/error_handler.dart
+++ lib/src/core/error/error_handler.dart
@@ -1,40 +1,48 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:hive/hive.dart';
 
 import '../config/l10n/generated/l10n.dart';
 import 'error_model.dart';
 
 class ErrorHandler{
 
-  static ErrorModel? handleError(DioError error){
+  static ErrorModel? handleError(dynamic error){
     ErrorModel? errorModel;
-    switch (error.type) {
-      case DioErrorType.connectTimeout:
-      case DioErrorType.sendTimeout:
-      case DioErrorType.receiveTimeout:
-      case DioErrorType.other:
-        errorModel = ErrorModel(
-          status: S.current.handle_error_status_connect_time_out,
-          message: S.current.handle_error_message_connect_time_out,
-        );
-        break;
-      case DioErrorType.cancel:
-        errorModel = ErrorModel(
-          status: S.current.handle_error_status_cancel,
-          message: S.current.handle_error_message_cancel,
-        );
-        break;
-      case DioErrorType.response:
-        if(error.response?.statusCode != HttpStatus.notFound){
-          errorModel = ErrorModel.fromJson(error.response?.data);
-        }else if(error.response?.statusCode == HttpStatus.notFound){
+    if(error is DioError) {
+      switch (error.type) {
+        case DioErrorType.connectTimeout:
+        case DioErrorType.sendTimeout:
+        case DioErrorType.receiveTimeout:
+        case DioErrorType.other:
           errorModel = ErrorModel(
-            status: error.response?.statusMessage,
-            message: error.message,
+            status: S.current.handle_error_status_connect_time_out,
+            message: S.current.handle_error_message_connect_time_out,
           );
-        }
-        break;
+          break;
+        case DioErrorType.cancel:
+          errorModel = ErrorModel(
+            status: S.current.handle_error_status_cancel,
+            message: S.current.handle_error_message_cancel,
+          );
+          break;
+        case DioErrorType.response:
+          if(error.response?.statusCode != HttpStatus.notFound){
+            errorModel = ErrorModel.fromJson(error.response?.data);
+          }else if(error.response?.statusCode == HttpStatus.notFound){
+            errorModel = ErrorModel(
+              status: error.response?.statusMessage,
+              message: error.message,
+            );
+          }
+          break;
+      }
+    }else if(error is HiveError){
+      errorModel = ErrorModel(
+        status: 'Fail',
+        message: error.message,
+      );
     }
     return errorModel;
   }
