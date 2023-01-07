b28d51e70ebe9652b6bf03198a6d565b4e3a9434
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2023-01-07 14:45:13 +0200

    Update category_news_item_widget.dart

--- lib/src/presentation/widgets/category_news_item_widget.dart
+++ lib/src/presentation/widgets/category_news_item_widget.dart
@@ -2,15 +2,24 @@ import 'package:aagel/src/core/config/themes/language_manager.dart';
 import 'package:aagel/src/data/models/article_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
 import '../../core/config/assets/assets.gen.dart';
 import '../../core/config/l10n/generated/l10n.dart';
 import '../../core/config/themes/theme_manager.dart';
+import '../controllers/data_bloc/api_data_bloc.dart';
 
-class CategoryNewsItemWidget extends StatelessWidget {
+class CategoryNewsItemWidget extends StatefulWidget {
   final ArticleModel article;
   const CategoryNewsItemWidget({super.key, required this.article});
+
+  @override
+  State<CategoryNewsItemWidget> createState() => _CategoryNewsItemWidgetState();
+}
+
+class _CategoryNewsItemWidgetState extends State<CategoryNewsItemWidget> {
+  final ApiDataBloc<ArticleModel> _bookmark = ApiDataBloc();
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +49,7 @@ class CategoryNewsItemWidget extends StatelessWidget {
                   height: double.infinity,
                   width: constraints.maxWidth * 0.35,
                   child: CachedNetworkImage(
-                    imageUrl: article.urlToImage ?? '',
+                    imageUrl: widget.article.urlToImage ?? '',
                     width: double.infinity,
                     height: double.infinity,
                     fit: BoxFit.cover,
@@ -67,13 +76,13 @@ class CategoryNewsItemWidget extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        article.title!,
+                        widget.article.title!,
                         softWrap: true,
                         overflow: TextOverflow.ellipsis,
                         maxLines: 2,
                       ),
                       Text(
-                        '${S().category_item_source} ${article.source?['name']}',
+                        '${S().category_item_source} ${widget.article.source?['name']}',
                         softWrap: true,
                         overflow: TextOverflow.ellipsis,
                         maxLines: 1,
@@ -86,22 +95,29 @@ class CategoryNewsItemWidget extends StatelessWidget {
                             Flexible(
                               child: Text(
                                 DateFormat('dd/MM/yyyy hh:mm a').format(
-                                    DateTime.tryParse(article.publishedAt!)!),
+                                    DateTime.tryParse(widget.article.publishedAt!)!),
                                 softWrap: true,
                                 overflow: TextOverflow.ellipsis,
                                 maxLines: 1,
                               ),
                             ),
-                            IconButton(
-                              onPressed: () {},
-                              padding: EdgeInsets.zero,
-                              constraints: const BoxConstraints(),
-                              splashColor: Colors.transparent,
-                              highlightColor: Colors.transparent,
-                              icon: const Icon(
-                                Icons.star_outline,
-                                size: 18,
-                              ),
+                            BlocBuilder<ApiDataBloc, ApiDataState>(
+                              bloc: _bookmark,
+                              builder: (context, state) {
+                                return IconButton(
+                                  onPressed: () {
+                                    _bookmark.add(ToggleSaveOrDelete(false, article: widget.article));
+                                  },
+                                  padding: EdgeInsets.zero,
+                                  constraints: const BoxConstraints(),
+                                  splashColor: Colors.transparent,
+                                  highlightColor: Colors.transparent,
+                                  icon: Icon(
+                                    (state is ApiDataLoaded<bool> && state.data != null && state.data == true) ? Icons.star_rate_rounded : Icons.star_outline,
+                                    size: 18,
+                                  ),
+                                );
+                              },
                             ),
                           ],
                         ),
@@ -109,7 +125,7 @@ class CategoryNewsItemWidget extends StatelessWidget {
                     ],
                   ),
                 ),
-              )
+              ),
             ],
           ),
         );
