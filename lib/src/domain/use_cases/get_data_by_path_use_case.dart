881c62c55bc5683e4853de908f68cb0cc09f6ff0
Mahmoud Ahmed <58610163+Mahm0ud-Ahmed@users.noreply.github.com>
2023-01-07 03:22:45 +0200

    Update get_data_by_path_use_case.dart

--- lib/src/domain/use_cases/get_data_by_path_use_case.dart
+++ lib/src/domain/use_cases/get_data_by_path_use_case.dart
@@ -9,7 +9,7 @@ import 'package:aagel/src/core/utils/data_state.dart';
 import 'package:aagel/src/core/utils/query_params.dart';
 import 'package:aagel/src/domain/repositories/i_app_repository.dart';
 
-class GetDataByPathUseCase<MODEL> extends UseCase{
+class GetDataByPathUseCase<MODEL> extends UseCase<QueryParams>{
 
   final IAppRepository _appRepository;
   DataReflection<MODEL>? _reflection;
@@ -17,10 +17,10 @@ class GetDataByPathUseCase<MODEL> extends UseCase{
   GetDataByPathUseCase(this._appRepository);
   
   @override
-  Future<DataState> call({required QueryParams params}) async {
+  Future<DataState> call({QueryParams? params}) async {
     _reflection = DataReflection();
     try {
-      HttpResponse response = await _appRepository.getShowDataByPath(params);
+      HttpResponse response = await _appRepository.getShowDataByPath(params!);
       if(response.response.statusCode == HttpStatus.ok){
         DataState dataModel = _reflection!.reflectResponse<MODEL>(response);
         return DataSuccess(dataModel.data);
